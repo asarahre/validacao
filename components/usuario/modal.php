b99737da14f3b3de7568c3050ab7d9033d93ccb6
asarahre <sarahbraga3433@gmail.com>
2022-09-26 21:26:03 -0400

    corrige erro id escrito errado

--- components/usuario/modal.php
+++ components/usuario/modal.php
@@ -46,7 +46,7 @@
           </div>
           <div class="flex justify-between">
           </div>
-          <button type="button" id="usario-form-salvar"
+          <button type="button" id="usuario-form-salvar"
             class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Salvar </button>
         </form>
